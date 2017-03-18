929d07a033cb87498086c5c479d8913e3466e421
Chantal Justamond <cjustamm66@me.com>
2017-03-18 12:56:00 -0600

    loop is working

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,28 +1,29 @@
 class BubbleSort
-  def initialize 
-    @letters_to_sort = ["d", "b", "a", "c"]
-    swaps = 0
-    @swapped = false
+  def initialize(letters_to_sort) 
+    @letters_to_sort = letters_to_sort
+    #swaps = 0
+    #@swapped = false
   end
   
   def sort
-    # number of times we'll loop through.
     n = @letters_to_sort.length
-
-    while @swapped == false
+     
+    # n-1 is number of times we'll loop through
+   loop do
+     @swapped = false
       (n-1).times do |index|
         if @letters_to_sort[index] > @letters_to_sort[index + 1]
           @letters_to_sort[index], @letters_to_sort[index + 1] = @letters_to_sort[index + 1], @letters_to_sort[index]
           @swapped = true  
           #swaps +=1
         end
-        
       end  
-      break if !swapped
+     break if !@swapped
     end
+  @letters_to_sort  
   end
   
 end
 
-sorter = BubbleSort.new
+sorter = BubbleSort.new(["d", "b", "a", "c"])
 p sorter.sort
