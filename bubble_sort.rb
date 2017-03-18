329912b46a0804d6810c373fc585804e6fd21be6
Chantal Justamond <cjustamm66@me.com>
2017-03-17 22:16:46 -0600

    initialize method

---
+++ bubble_sort.rb
@@ -0,0 +1,18 @@
+class BubbleSort
+  def initialize 
+    @numbers_to_sort = ["d", "b", "a", "c"]
+    swaps = 0
+    
+  end
+  
+  def sort
+    
+    while 
+   
+    end
+  end
+  
+end
+
+sorter = BubbleSort.new
+p sorter.sort
