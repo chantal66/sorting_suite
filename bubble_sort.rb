ed5be4bd9c4040c13e4d2e381ab67a4cda7f464d
Chantal Justamond <cjustamm66@me.com>
2017-03-17 22:42:21 -0600

    while loop not working yet

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,14 +1,24 @@
 class BubbleSort
   def initialize 
-    @numbers_to_sort = ["d", "b", "a", "c"]
+    @letters_to_sort = ["d", "b", "a", "c"]
     swaps = 0
-    
+    @swapped = false
   end
   
   def sort
+    # number of times we'll loop through
+    n = @letters_to_sort.length
     
-    while 
-   
+    while @swapped == false
+      (n-1).times do |index|
+        if @letters_to_sort[index] > @letters_to_sort[index + 1]
+          @letters_to_sort[index], @letters_to_sort[index + 1] = @letters_to_sort[index + 1], @letters_to_sort[index]
+          @swapped = true  
+          #swaps +=1
+        end
+        
+      end  
+      break if !swapped
     end
   end
   
